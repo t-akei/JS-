f2f83583445c9f62ceaa07e8f8c3144ad42c77c9
t-akei <145541870+t-akei@users.noreply.github.com>
2023-12-09 15:00:48 +0000

    [JS演習]

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
+let hello = 'Hello World';
+alert(hello);
+
+let int1 = 1;
+alert(int1);
+
+alert(4 + 3);
+
+alert('Hello' + 'World');
+
+
 let str1 = 'Hello';
-let str2 = 'World!';
+let str2 = 'World!!';
 alert(str1 + str2);
+
+
+let orange = 100;
+let apple = 120;
+
+if(orange<apple){
+    alert('みかんの値段がリンゴより安い');
+} else if(orange == apple){
+    alert('みかんとりんごが同じ値段');
+} else{
+    alert('みかんの値段がりんごより高い');
+}
