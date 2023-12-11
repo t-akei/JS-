14d640fc9b5e26ce874a6056ac08d4e30cea959e
t-akei <145541870+t-akei@users.noreply.github.com>
2023-12-11 07:47:48 +0000

    [JS演習]

--- app.js
+++ app.js
@@ -133,15 +133,20 @@ alert(promptStr);
     // return js_hand;
 // }
 
-
+// じゃんけんの手を入力してもらうプロンプト欄を生成
 let user_hand = prompt('じゃんけんの手をグー、チョキ、パーから選んでください');
 
+// じゃんけんの手をランダムに作成する関数を呼び出す
 let js_hand = getJShand();
 
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数を呼び出し
+// 結果をjudgeに入れる
 let judge = winLose(user_hand, js_hand);
 
+// 結果を表示する
 alert('あなたの選んだ手は' + user_hand + 'です。\nJavaScriptの選んだ手は' + js_hand + 'です。\n結果は' + judge + 'です。');
 
+// ランダムでじゃんけんの手を作成する関数
 function getJShand(){
     let js_hand_num = Math.floor(Math.random() * 3);
     let hand_name;
@@ -158,6 +163,7 @@ function getJShand(){
 }
 
 
+// ユーザの手とJavaScriptのじゃんけんの手を比べる関数
 function winLose(user, js){
     let winLoseStr;
     
